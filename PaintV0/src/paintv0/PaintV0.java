ba65859ccbb8ced7f335a8405480f43813d2c9da
Joe Leveille <joe.leveille@valpo.edu>
2019-08-29 17:49:02 -0500

    Switched to grid layout

--- PaintV0/src/paintv0/PaintV0.java
+++ PaintV0/src/paintv0/PaintV0.java
@@ -9,13 +9,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import static java.lang.System.gc;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -52,6 +53,14 @@ public class PaintV0 extends Application {
     public void start(Stage primaryStage) {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
+        
+        GridPane gridPane = new GridPane(); //Create the blank grid
+        gridPane.setMinSize(400, 400);  //and set it's attributes
+        gridPane.setVgap(5); 
+        gridPane.setHgap(5);       
+        gridPane.setAlignment(Pos.CENTER); 
+        
+        
         /*btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
@@ -82,13 +91,12 @@ public class PaintV0 extends Application {
         final Menu menu3 = new Menu("Help");
         topMenu.getMenus().addAll(menu1, menu2, menu3);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        root.getChildren().add(topMenu);
+        gridPane.add(btn, 3, 2);
+        gridPane.add(topMenu, 0, 0);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(gridPane); //Placing the grid on the screen
+
+        primaryStage.setTitle("Paint v0"); //Set the window title
         primaryStage.setScene(scene);
         primaryStage.show();
     }
